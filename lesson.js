78c24747c75109c9f3c78b393ea1acd60ffa93f8
esewi osemwegie <kulboy900@gmail.com>
2025-03-17 12:05:43 +0100

    modicato-inizio

--- lesson.js
+++ lesson.js
@@ -5,11 +5,11 @@ console.log(num1);
 let num2 = '4'; 
 console.log(num2);
 
-let string = num1 + num2;
-console.log(string);
+let str = num1 + num2;
+console.log(str);
 
-string = '67';
-console.log(string);
+str = 'number';
+console.log(str);
 
 const PIGRECO ='3.14159265359';
 PIGRECO = 200;
