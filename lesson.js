05924ddfff167a8b40e8dfd427f00487005893f7
esewi osemwegie <kulboy900@gmail.com>
2025-03-17 11:26:57 +0100

    inizio

---
+++ lesson.js
@@ -0,0 +1,32 @@
+
+let num1 =' 12';
+console.log(num1);
+
+let num2 = '4'; 
+console.log(num2);
+
+let string = num1 + num2;
+console.log(string);
+
+string = '67';
+console.log(string);
+
+const PIGRECO ='3.14159265359';
+PIGRECO = 200;
+console.log(PIGRECO)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
